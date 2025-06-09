da1e7f5b1eaa1703101709cb4fc863d89d7843b9
Jeremy Fowler <jembyfowler@gmail.com>
2025-06-09 12:15:37 -0600

    monster template

--- app/models/Monster.js
+++ app/models/Monster.js
@@ -9,4 +9,42 @@ export class Monster {
     this.imgUrl = data.image
     this.name = data.name
   }
+
+  get cardHTMLTemplate() {
+    return /*html*/`
+    <div class="col-md-6">
+      <div class="card">
+        <img src="https://botw-compendium.herokuapp.com/api/v3/compendium/entry/water_octorok/image?game=totk"
+          class="card-img-top" alt="water octorok">
+        <div class="card-body">
+          <div class="d-flex gap-3">
+            <p class="card-title fs-5 fw-bold">${this.name}</p>
+            <div class="fs-3" title="DLC monster">
+              <span class="mdi mdi-cash-100"></span>
+              <span class="mdi mdi-download"></span>
+            </div>
+          </div>
+          <p class="card-text">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta nam itaque necessitatibus nostrum
+            dolorem aperiam fugit dolor, est beatae voluptate.
+          </p>
+          <div>
+            <b>Locations</b>
+            <ul>
+              <li>The lake</li>
+              <li>The desert</li>
+            </ul>
+          </div>
+          <div>
+            <b>Loot Drops</b>
+            <ul>
+              <li>tentacle</li>
+              <li>eyeball</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+  }
 }
