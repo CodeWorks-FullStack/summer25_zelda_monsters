a9c464aa4d5fe657e4dc5022d19c6423a1cc6b96
Jeremy Fowler <jembyfowler@gmail.com>
2025-06-09 11:56:44 -0600

    added monsters array to appstate

--- app/AppState.js
+++ app/AppState.js
@@ -2,7 +2,8 @@ import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
-
+  // NOTE array will be populated with data after our network request
+  monsters = []
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
